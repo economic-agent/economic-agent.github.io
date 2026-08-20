5fedd1f4998d32aefc2de6a431bc1010f5d6e99e
economicagent <economicagent@fastmail.com>
2026-08-20 15:08:58 +0000

    fix: http scheme for api.economicagent.net:8080 (server has no TLS on 8080; https variant times out)

--- watch_payments.py
+++ watch_payments.py
@@ -21,7 +21,7 @@ USDC_ETH = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"
 USDT_ETH = "0xdAC17F958D2ee523a2206206994597C13D831ec7"
 BALANCE_SELECTOR = "0x70a08231000000000000000000000000" + EVM_ADDR[2:].lower()
 
-EVM_RPCS = ["https://ethereum-rpc.publicnode.com", "https://eth.llamarpc.com"]
+EVM_RPCS = ["https://mainnet.gateway.tenderly.co", "https://rpc.flashbots.net"]
 SOL_RPC = "https://api.mainnet-beta.solana.com"
 
 
